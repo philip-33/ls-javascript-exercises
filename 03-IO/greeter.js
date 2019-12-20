851cfa44cce2d40d02e6158d1f8e53ba744ec9d8
dev <22357184+philip-33@users.noreply.github.com>
2019-12-19 19:06:56 -0800

    Chapter 3

--- 03-IO/greeter.js
+++ 03-IO/greeter.js
@@ -1,6 +1,4 @@
-// greeter.js
-function greet(fname, lname) {
-  console.log('Hello, ' + fname + ' ' + lname + '!');
-};
-
-greet('Mister', 'Magoo');
+let inputLine = require('readline-sync');
+let fname = inputLine.question('What is your first name? ');
+let lname = inputLine.question('What is your last name? ');
+console.log(`Hello, ${fname} ${lname}!`);
