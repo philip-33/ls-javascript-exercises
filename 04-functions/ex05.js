e996a7100de9382f13728d3c934aabb9e849ac0a
dev <22357184+philip-33@users.noreply.github.com>
2019-12-22 02:02:32 -0800

    Chapter 4 cont'd.

---
+++ 04-functions/ex05.js
@@ -0,0 +1,9 @@
+function scream(words) {
+  words = words + '!!!!';
+  return;
+  console.log(words);
+}
+
+scream('Yipeee');
+
+// The function returns before logging the text so nothing happens
---
+++ 04-functions/ex06.js
@@ -0,0 +1,7 @@
+function scream(words) {
+  return words + '!!!!';
+}
+
+scream('Yipeee');
+
+// this does not log anything because there is no console.log statement
